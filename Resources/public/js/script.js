cea5226cb31142167d4f752a4bb7908124f30981
Nizar Khalife Iglesias <nizar@nobox.com>
2012-04-12 12:47:30 -0400

    Added js object literals and DOM-ready execution of methods.
    This follows the Garber-Irish Implementation as described in http://viget.com/inspire/extending-paul-irishs-comprehensive-dom-ready-execution.
    
    Signed-off-by: Nizar Khalife Iglesias <nizar@nobox.com>

--- Resources/public/js/script.js
+++ Resources/public/js/script.js
@@ -3,6 +3,57 @@
 */
 
 
+APPBOILERPLATEBUNDLE = {
+    // !Common
+    common: {
+        init: function()
+        {
+            // application-wide code
+        }
+    },  // End common
+
+    // !Controller
+    appboilerplate: {
+        init: function()
+        {
+            // controller-wide code
+        },
+
+        hello: function()
+        {
+            // action-specific code
+            alert ($('h1').text());
+        }
+    }  // End controller
+};  // End SITENAME
 
 
+// !UTIL
+UTIL = {
+    controller: "",
+    action:     "",
 
+    exec: function (controller, action)
+    {
+        var ns     = APPBOILERPLATEBUNDLE,
+            action = (action === undefined) ? "init" : action;
+
+        if (controller !== "" && ns[controller] && typeof ns[controller][action] == "function") {
+            ns[controller][action]();
+        }
+    },
+
+    init: function()
+    {
+        var body        = document.body;
+        UTIL.controller = body.getAttribute ("data-controller"),
+        UTIL.action     = body.getAttribute ("data-action");
+
+        UTIL.exec ("common");
+        UTIL.exec (UTIL.controller);
+        UTIL.exec (UTIL.controller, UTIL.action);
+    }
+};  // End UTIL
+
+
+$(document).ready (UTIL.init);
